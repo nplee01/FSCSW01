062e95da93dc3a4971abbf03eb7289d84b672ee9
ZimSpring <simcc63@gmail.com>
2021-06-14 16:27:22 +0800

    Add volume for db

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,7 +4,7 @@
 # Expects to be run in app home (where manage.py is)
 
 # Wait for DB first, then create DB tables, then create app data, then run gunicorn wsgi server
-python manage.py wait_for_db && \
+python manage.py wait_for_database && \
 python manage.py migrate && \
 python manage.py appdata && \
 gunicorn --bind 0.0.0.0:3000 --workers 5 backtest.wsgi:application
